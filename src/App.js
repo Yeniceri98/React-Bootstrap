333f833b041f1598dd00e98718fae144281771e1
Ahmet Sami <ahmetsamiyeniceri@gmail.com>
2020-10-11 15:03:06 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            ads
+        </div>
+    );
+}
+
+export default App;
+
+
+/*
+    _____ REACT BOOTSTRAP SETUP _____
+    https://react-bootstrap.github.io/getting-started/introduction/
+    npm install react-bootstrap bootstrap
+*/
+
